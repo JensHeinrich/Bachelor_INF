1aa35617d039b1b7f5d4cabe420b40d2337a420a
Jens Heinrich <github.com/JensHeinrich>
2022-09-16 10:14:54 +0200

    Create exception proof peak method and clean up

--- src/oaipmh/publishers.py
+++ src/oaipmh/publishers.py
@@ -1,11 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
+from typing import Union
 
 import datasets
+import requests
+from bs4 import BeautifulSoup
+from requests.models import CaseInsensitiveDict
 
 from .xml_loader import OAIXMLRecordDict
 
 logger = datasets.utils.logging.get_logger(__name__)
+
+
+def _peak_at_link(
+    link: str,
+) -> Union[requests.Response, None]:  # TODO replace with _peak_at_links
+    """Peak at the provided link in an exception safe way
+
+    Args:
+        link (str): url to peak at
+
+    Returns:
+        Union[requests.Response,None]: Response of the request or None if it failed
+    """
+    req = None
+    try:
+        req = requests.head(link)
+    except Exception as N:
+        logger.warning(f"Exception occured handling {link}: {N}")
+
+    return req
 
 
 def get_pdf_links(record: OAIXMLRecordDict, publisher: str = "") -> list[str]:
@@ -28,11 +50,14 @@ def get_pdf_links(record: OAIXMLRecordDict, publisher: str = "") -> list[str]:
             req.url
             for rel in record["relations"]
             if (
-                req := requests.head(  # save the request in a temporary variable  # only do a HEAD not a GET; no need to download the pdf now
-                    rel.replace("index.php/Language%20Science%20Press/", "")
+                (
+                    req := _peak_at_link(  # save the request in a temporary variable  # only do a HEAD not a GET; no need to download the pdf now
+                        rel.replace("index.php/Language%20Science%20Press/", "")
+                    )
                 )
-            ).headers["Content-Type"]
-            == "application/pdf"  # only get links pointing to pdf
+                and req.headers["Content-Type"]
+                == "application/pdf"  # only get links pointing to pdf
+            )
         ] or [
             # fall back to extracting the urls from the page
             req.url
@@ -40,16 +65,21 @@ def get_pdf_links(record: OAIXMLRecordDict, publisher: str = "") -> list[str]:
                 ".item.files > .pub_format_single > .pdf"
             )
             if (
-                req := requests.head(  # save the request in a temporary variable
-                    href
-                    if isinstance(
-                        str,
-                        href := a["href"],  # pyright: ignore [reportGeneralTypeIssues]
-                    )
-                    else href[0]  # pyright: ignore [reportGeneralTypeIssues]
-                )  # only do a HEAD not a GET; no need to download the pdf now
-            ).headers["Content-Type"]
-            == "application/pdf"  # only get links pointing to pdf
+                (
+                    req := _peak_at_links(  # save the request in a temporary variable
+                        href
+                        if isinstance(
+                            str,
+                            href := a[
+                                "href"
+                            ],  # pyright: ignore [reportGeneralTypeIssues]
+                        )
+                        else href[0]  # pyright: ignore [reportGeneralTypeIssues]
+                    )  # only do a HEAD not a GET; no need to download the pdf now
+                )
+                and req.headers["Content-Type"]
+                == "application/pdf"  # only get links pointing to pdf
+            )
         ]
 
     else:
